541092d2ef555c165d4756ed9fd57493bfede306
Fernando Pozo <fernando.pozo@nanuktech.cl>
2023-12-13 13:11:23 -0300

    move providers

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -4,3 +4,23 @@ provider "google" {
   zone        = var.zone
   credentials = var.service_account_json_path
 }
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+  required_version = ">= 0.13"
+
+  backend "gcs" {
+    bucket = "tf-state-example-arauco"
+    prefix = "terraform/state"
+    credentials = "#{service_account_json_path}#"
+  }
+
+}
